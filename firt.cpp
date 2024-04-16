e47b187269968cdf50ed9a76a6dd2cd5f5b9d627
devJawad1 <adekanbija1@gmail.com>
2024-04-16 15:52:15 +0100

    my first c++ class

---
+++ firt.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+    // string fname;
+    // cout << "Enter your firstname ";
+    // getline(cin , fname);
+    // string output =  "Welcome "+ fname + " to c++ class" ;
+    // cout<< output << std::endl;
+
+    // string level;
+    // cout << "Enter your level ";
+    // getline(cin , level);
+    // string lname , location, myclass;
+    // cout << "Enter your lastname ";
+    // getline(cin, lname);
+    // cout << "Enter your location ";
+    // getline (cin, location);
+    // cout << "Enter your Programe ";
+    // getline (cin, myclass);
+    // cout << "Welcome "<< fname <<" "<< lname << " your are currently living in " << location << ", you are running " << myclass << " program in SQI college" <<std::endl;
+
+    cout << "Enter name of your state";
+    string state
+    getline(cin, state)
+
+    if(state== "Oyo" || state== "Ogun" || state=="Osun"|| state=="Ekiti"){
+        cout <<" This is Yoruba tribe "
+    }
+    return 0;
+}
+
+// write a simple application that can be use to caheck wether condition if rain is going to fall base on some parameter
